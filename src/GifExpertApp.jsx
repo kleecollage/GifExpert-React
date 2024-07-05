2029160c3899c6876a91b9282ee9dc162aa1fad8
Klee Collage <klee.collage@gmail.com>
2024-07-05 16:20:49 -0500

    components and styles added
    files added: GifGrid, AddCategory, styles (global), integrated in GifsExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch Man",
+    "Chainsaw Man",
+  ]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
   return (
-    <h1>GifExpertApp</h1>
-  )
-}
+    <>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+      {/*  Input  */}
+      <AddCategory onNewCategory={onAddCategory} />
+      {/* Listado de gIF */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
